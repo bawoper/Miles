9a6a9c9e0dea4f453e2d2906aced24117bdc6b27
Andrey <bawoper@gmail.com>
2024-08-14 15:18:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int TicketPrice = 687;
+        int Bonusdrop = 20;
+        int Miles = TicketPrice / Bonusdrop;
+
+
+        System.out.println(Miles + "_Miles");
+    }
+}
